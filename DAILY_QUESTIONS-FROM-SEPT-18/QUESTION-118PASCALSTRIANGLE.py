882cebea4bfccaf7bf31334f8607d29923082ddd
prashanthi <prashmudepaka@gmail.com>
2024-09-30 05:29:20 +0530

    pascals triangle 118 changed to cpp

--- DAILY_QUESTIONS-FROM-SEPT-18/QUESTION-118PASCALSTRIANGLE.py
+++ DAILY_QUESTIONS-FROM-SEPT-18/QUESTION-118PASCALSTRIANGLE.py
@@ -21,6 +21,24 @@ Output: [[1]]
 Constraints:
 
 1 <= numRows <= 30'''
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> r(numRows);
+        for(int i=0;i<numRows;i++)
+        {
+            r[i].resize(i+1);
+            r[i][0]=r[i][i]=1;
+            for(int j=1;j<i;j++)
+            {
+                r[i][j]=r[i-1][j-1]+r[i-1][j];
+            }
+        }
+        return r;
+    }
+};
+
 class Solution(object):
     def generate(self, numRows):
         l = [[0, 1, 0]]
